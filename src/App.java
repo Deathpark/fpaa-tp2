f2a9b2ca892f3aba4b45efab399d8723c7a6d7a1
Leticia <leticiafraga55@gmail.com>
2022-11-17 21:22:10 -0300

    fixes

--- src/App.java
+++ src/App.java
@@ -3,47 +3,35 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class App {
+
+    // Usando o caminhão com maior soma de rotas como diferença
     public static void main(String[] args) throws Exception {
         LinkedList<Rota> rotas = new LinkedList<>();
-        LinkedList<Caminhao> caminhoes = new LinkedList<>();
         int quant = (int) (Math.random() * 20);
         for (int i = 0; i < 1000; i++) {
             rotas.add(new Rota(i, (Math.random() * 100)));
         }
+        algGuloso((LinkedList<Rota>) rotas.clone(), novaListaCaminhoes(quant));
+    }
+
+    public static LinkedList<Caminhao> novaListaCaminhoes(int quant) {
+        LinkedList<Caminhao> caminhoes = new LinkedList<>();
         for (int i = 0; i < quant; i++) {
             caminhoes.add(new Caminhao());
         }
-        algGuloso((LinkedList<Rota>) rotas.clone(), (LinkedList<Caminhao>) caminhoes.clone());
+        return caminhoes;
     }
 
-    public static void algGuloso(List<Rota> rotas, List<Caminhao> caminhoes) {
+    public static double algGuloso(List<Rota> rotas, List<Caminhao> caminhoes) {
         Collections.sort(rotas);
         for (Rota rota : rotas) {
             Caminhao min = caminhoes.stream().min((c1, c2) -> c1.compareTo(c2)).get();
+            System.out.println(min.getSoma());
             min.rotas.add(rota);
         }
-        desvioPadrao(caminhoes);
+        Caminhao max = caminhoes.stream().max((c1, c2) -> c1.compareTo(c2)).get();
+        System.out.println(max.getSoma());
+        return max.getSoma();
     }
 
-    /**
-     * Calcula o desvio padrão entre os caminhões
-     * 
-     * @param caminhoes
-     * @return
-     */
-    public static double desvioPadrao(List<Caminhao> caminhoes) {
-        double media = 0;
-        for (Caminhao caminhao : caminhoes) {
-            media += caminhao.getSoma();
-        }
-        media /= caminhoes.size();
-        double diferenca = 0;
-        for (Caminhao caminhao : caminhoes) {
-            diferenca += Math.pow(caminhao.getSoma() - media, 2);
-        }
-        diferenca = Math.sqrt(diferenca / caminhoes.size());
-        System.out.println("DIFERENCA");
-        System.out.println(diferenca);
-        return diferenca;
-    }
 }
